b5c34f0308cf0d72fbdd4300f0f64e65de5cf5b2
tqduc1 <104497178+tqduc1@users.noreply.github.com>
2022-05-06 10:41:18 +0700

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -6,12 +6,12 @@ rm -rf build/
 
 ./gradlew bootJar -DskipTests
 
-docker container stop $(docker ps -aqf "name=^office-backend-uat")
+docker container stop $(docker ps -aqf "name=^office-backend")
 
-docker container rm $(docker ps -aqf "name=^office-backend-uat")
+docker container rm $(docker ps -aqf "name=^office-backend")
 
-docker rmi $(docker images 'tdx-images/office-backend-uat' -q)
+docker rmi $(docker images 'tdx-images/office-backend' -q)
 
-docker build -t tdx-images/office-backend-uat:$TAG -f $DIR/Dockerfile $DIR
+docker build -t tqduc1/office-backend:$TAG -f $DIR/Dockerfile $DIR
 
-docker tag tdx-images/office-backend-uat:latest tdx-images/office-backend-uat:$TAG
+docker tag tqduc1/office-backend:latest tqduc1/office-backend:$TAG
